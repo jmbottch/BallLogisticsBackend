7926b467c67805c910aef6d8419798df5598a4ee
Jeroen Bottcher <jmbottch@avans.nl>
2020-05-12 11:36:51 +0200

    fixed messages to RabbitMQ for all operations

--- src/controllers/logisticsCompanyController.js
+++ src/controllers/logisticsCompanyController.js
@@ -1,88 +1,146 @@
 const LogisticsCompany = require('../models/logisticsCompany')
-const amqp = require('amqplib')
-
-//TODO: SEND MESSAGE WHEN COMPANY IS CREATED
-//TODO: SEND MESSAGE WHEN COMPANY IS EDITED
-//TODO: SEND MESSAGE WHEN COMPANY IS REMOVED
+const amqp = require('amqplib/callback_api')
 
 module.exports = {
 
-    getAll (req, res) {
+    getAll(req, res) {
         LogisticsCompany.find()
-        .then((companies) => {
-            res.status(200).send(companies)
-        })
-        .catch((err) => {
-            res.status(404).send(err)
-        })
+            .then((companies) => {
+                res.status(200).send(companies)
+            })
+            .catch((err) => {
+                res.status(404).send(err)
+            })
     },
 
-    getOne(req,res) {
-        LogisticsCompany.findOne({companyName : req.body.companyName})
-        .then((company) => {
-            res.status(200).send(company)
-        })
-        .catch((err) => {
-            res.status(404).send({Message : "Company was not found."})
-        })
+    getOne(req, res) {
+        LogisticsCompany.findOne({ companyName: req.body.companyName })
+            .then((company) => {
+                res.status(200).send(company)
+            })
+            .catch((err) => {
+                res.status(404).send({ Message: "Company was not found." })
+            })
     },
 
-    create(req,res) {
+    create(req, res) {
         LogisticsCompany.create({
-            companyName : req.body.companyName,
-            smallPackageDeliveryPrices : req.body.smallPackageDeliveryPrices,
-            mediumPackageDeliveryPrices : req.body.mediumPackageDeliveryPrices,
-            largePackageDeliveryPrices : req.body.largePackageDeliveryPrices
+            companyName: req.body.companyName,
+            smallPackageDeliveryPrices: req.body.smallPackageDeliveryPrices,
+            mediumPackageDeliveryPrices: req.body.mediumPackageDeliveryPrices,
+            largePackageDeliveryPrices: req.body.largePackageDeliveryPrices
         })
-        .then((company) => {  
-            res.status(200).send({Message : "Company has been created"})
-        })
-        .catch((err) => {
-            if (err.name == 'MongoError' && err.code == 11000) {
-                res.status(401).send({Error: "This company is already registered."})
-            } else {
-                res.status(402).send({Error : err})
-            }
-        })
+            .then((company) => {
+                amqp.connect('amqp://localhost', (error0, connection) => {
+                    if (error0) throw error0
+                    else {
+                        connection.createChannel((error1, channel) => {
+                            if (error1) throw error1
+                            else {
+                                var queue = 'logistics-register'
+
+                                var message = {
+                                    companyName: company.companyName,
+                                    smallPackageDeliveryPrices: company.smallPackageDeliveryPrices,
+                                    mediumPackageDeliveryPrices: company.mediumPackageDeliveryPrices,
+                                    largePackageDeliveryPrices: company.largePackageDeliveryPrices
+                                }
+                                var payload = JSON.stringify(message)
+
+                                channel.assertQueue(queue, { durable: true })
+                                console.log("Message sent")
+                                channel.sendToQueue(queue, Buffer.from(payload))
+                                res.status(200).send({ Message: "Company registered and message sent to bus" })
+                            }
+                        })
+                    }
+                })
+            })
+            .catch((err) => {
+                if (err.name == 'MongoError' && err.code == 11000) {
+                    res.status(401).send({ Error: "This company is already registered." })
+                } else {
+                    res.status(402).send({ Error: err })
+                }
+            })
     },
 
     update(req, res) {
-        LogisticsCompany.findOne({companyName : req.body.companyName})
-        .then((company) => {
-            let smallPricesToSet = req.body.smallPackageDeliveryPrices
-            let mediumPricesToSet = req.body.mediumPackageDeliveryPrices
-            let largePricesToSet = req.body.largePackageDeliveryPrices
+        LogisticsCompany.findOne({ companyName: req.body.companyName })
+            .then((company) => {
+                let smallPricesToSet = req.body.smallPackageDeliveryPrices
+                let mediumPricesToSet = req.body.mediumPackageDeliveryPrices
+                let largePricesToSet = req.body.largePackageDeliveryPrices
 
-            if(req.body.smallPackageDeliveryPrices == '' || req.body.smallPackageDeliveryPrices == null || req.body.smallPackageDeliveryPrices === []) smallPricesToSet = company.smallPackageDeliveryPrices
-            if(req.body.mediumPackageDeliveryPrices == '' || req.body.mediumPackageDeliveryPrices == null | req.body.mediumPackageDeliveryPrices === []) mediumPricesToSet = company.mediumPackageDeliveryPrices
-            if(req.body.largePackageDeliveryPrices == '' || req.body.largePackageDeliveryPrices == null || req.body.largePackageDeliveryPrices === []) largePricesToSet = company.largePackageDeliveryPrices
+                if (req.body.smallPackageDeliveryPrices == '' || req.body.smallPackageDeliveryPrices == null || req.body.smallPackageDeliveryPrices === []) smallPricesToSet = company.smallPackageDeliveryPrices
+                if (req.body.mediumPackageDeliveryPrices == '' || req.body.mediumPackageDeliveryPrices == null | req.body.mediumPackageDeliveryPrices === []) mediumPricesToSet = company.mediumPackageDeliveryPrices
+                if (req.body.largePackageDeliveryPrices == '' || req.body.largePackageDeliveryPrices == null || req.body.largePackageDeliveryPrices === []) largePricesToSet = company.largePackageDeliveryPrices
 
-            company.set({
-                smallPackageDeliveryPrices : smallPricesToSet,
-                mediumPackageDeliveryPrices : mediumPricesToSet,
-                largePackageDeliveryPrices : largePricesToSet
-            })
-            company.save()
-            .then(() => {
-                res.status(200).send({Message : 'Company has been edited'})
+                company.set({
+                    smallPackageDeliveryPrices: smallPricesToSet,
+                    mediumPackageDeliveryPrices: mediumPricesToSet,
+                    largePackageDeliveryPrices: largePricesToSet
+                })
+                company.save()
+                    .then(() => {
+                        amqp.connect('amqp://localhost', (error0, connection) => {
+                            if (error0) throw error0
+                            connection.createChannel((error1, channel) => {
+                                if (error1) throw error1
+                                var queue = "logistics-edit"
+                                // var msg = req.body
+                                var message = {
+                                    companyName: company.companyName,
+                                    smallPackageDeliveryPrices: smallPricesToSet,
+                                    mediumPackageDeliveryPrices: mediumPricesToSet,
+                                    largePackageDeliveryPrices: largePricesToSet
+                                }
+                                let payload = JSON.stringify(message)
+                                channel.assertQueue(queue, {
+                                    durable: true
+                                })
+                                channel.sendToQueue(queue, Buffer.from(payload),{noAck: false})
+                                console.log("message for editing" + company.companyName + " sent to bus")
+                                res.status(200).send({ result: "Company Registered and message added to the queue" })
+                            })
+                        })                       
+                    })
+                    .catch((err) => {
+                        res.status(401).send(err)
+                    })
             })
             .catch((err) => {
-                res.status(401).send(err)
+                res.status(404).send({ Error: "Company not found" })
             })
-        })
-        .catch((err) => {
-            res.status(404).send({Error : "Company not found"})
-        })
     },
 
-    remove(req,res) {
-        LogisticsCompany.findOne({companyName : req.body.companyName})
-        .then((company) => {
-            company.remove()
-            res.status(200).send({Message : "Company has been removed"})
-        })
-        .catch((err) => {
-            res.status(404).send({Error : "Company not found"})
-        })
+    remove(req, res) {
+        LogisticsCompany.findOne({ companyName: req.body.companyName })
+            .then((company) => {
+                company.remove()
+                amqp.connect('amqp://localhost', (error0, connection) => {
+                    if(error0) throw error0
+                    else {
+                        connection.createChannel((error1, channel) => {
+                            if(error1) throw error1
+                            else {
+                                var queue = 'logistics-remove'
+                                var message = {
+                                    companyName : req.body.companyName
+                                }
+                                var payload = JSON.stringify(message)
+
+                                channel.assertQueue(queue, {durable: false})
+                                channel.sendToQueue(queue, Buffer.from(payload))
+                                console.log("Message for removing " + req.body.companyName + " was sent to the bus")
+                                res.status(200).send({Message : "Company removed and message sent to bus"})
+                            }
+                        })
+                    }
+                })
+            })
+            .catch((err) => {
+                res.status(404).send({ Error: "Company not found" })
+            })
     }
 }
