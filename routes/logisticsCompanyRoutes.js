1e7e4994e129532f07ec080d8908c67a8e809392
Jeroen Bottcher <jmbottch@avans.nl>
2020-05-16 20:40:52 +0200

    fixed issue

--- routes/logisticsCompanyRoutes.js
+++ routes/logisticsCompanyRoutes.js
@@ -1,11 +1,12 @@
 const logisitcsCompanyController = require('../src/controllers/logisticsCompanyController')
 
-module.exports = (app) => {
+module.exports = (app) => {   
+  
     //get list of all Logistics Companies
     app.get('/api/companies', logisitcsCompanyController.getAll)
     
     //get data of a single Logistics Company
-    app.get('/api/companies', logisitcsCompanyController.getOne)
+    app.get('/api/company', logisitcsCompanyController.getOne)
 
     //create a new Logistics Company
     app.post('/api/companies', logisitcsCompanyController.create)
